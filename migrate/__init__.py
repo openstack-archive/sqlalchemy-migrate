728b677e56a8b51c5bda6c127be9837db2c11160
jan.dittberner <unknown>
2009-01-25 12:52:13 +0000

    move .. automodule stuff to docs/api.rst

--- migrate/__init__.py
+++ migrate/__init__.py
@@ -1,14 +1,6 @@
 """
-SQLAlchemy Migrate
-------------------
-
-SQLAlchemy migrate provides two APIs :mod:`migrate.versioning` for database
-schema version and repository management and :mod:`migrate.changeset` that
-allows to define database schema changes using Python.
-
-.. automodule:: migrate.changeset
-   :members:
-
-.. automodule:: migrate.versioning
-   :members:
+   SQLAlchemy migrate provides two APIs :mod:`migrate.versioning` for
+   database schema version and repository management and
+   :mod:`migrate.changeset` that allows to define database schema changes
+   using Python.
 """
--- migrate/changeset/__init__.py
+++ migrate/changeset/__init__.py
@@ -1,22 +1,8 @@
 """
-Module :mod:`migrate.changeset`
-===============================
+   This module extends SQLAlchemy and provides additional DDL [#]_
+   support.
 
-This module extends SQLAlchemy and provides additional DDL [#]_ support.
-
-.. [#] SQL Data Definition Language
-
-.. automodule:: migrate.changeset.ansisql
-   :members:
-.. automodule:: migrate.changeset.constraint
-   :members:
-.. automodule:: migrate.changeset.databases
-   :synopsis: database specific changeset code
-   :members:
-.. automodule:: migrate.changeset.exceptions
-   :members:
-.. automodule:: migrate.changeset.schema
-   :members:
+   .. [#] SQL Data Definition Language
 """
 from migrate.changeset.schema import *
 from migrate.changeset.constraint import *

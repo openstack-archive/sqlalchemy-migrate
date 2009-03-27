77c1cae8a94714a24dcf464c7d62394409ce64e9
jan.dittberner <unknown>
2009-03-27 20:39:41 +0000

    start work on 0.5.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_requirements = ['nose >= 0.10']
 
 setup(
     name = "sqlalchemy-migrate",
-    version = "0.5.3",
+    version = "0.5.4",
     packages = find_packages(exclude=['test*']),
     include_package_data = True,
     description = "Database schema migration for SQLAlchemy",

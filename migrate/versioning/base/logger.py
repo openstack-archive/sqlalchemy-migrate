2687bf8e143fb19756e6066ec4494df102951d0d
iElectric <unknown>
2009-08-07 15:58:12 +0200

    removing deprecated logger module

--- migrate/versioning/base/logger.py
+++
@@ -1,9 +0,0 @@
-"""Manages logging (to stdout) for our versioning system.
-"""
-import logging
-
-log=logging.getLogger('migrate.versioning')
-log.setLevel(logging.WARNING)
-log.addHandler(logging.StreamHandler())
-
-__all__ = ['log','logging']

453d4bcf482510d7ff9bc80c9641692b34f1cd36
Rick Copeland <rick446@usa.net>
2009-08-12 15:00:19 -0400

    Add table declarations for tables with diff

--- migrate/versioning/genmodel.py
+++ migrate/versioning/genmodel.py
@@ -125,11 +125,11 @@ class ModelGenerator(object):
 
     def toUpgradeDowngradePython(self, indent='    '):
         ''' Assume model is most current and database is out-of-date. '''
-
         decls = ['from migrate.changeset import schema',
                  'meta = MetaData(migrate_engine)']
         for table in self.diff.tablesMissingInModel + \
-                self.diff.tablesMissingInDatabase:
+                self.diff.tablesMissingInDatabase  + \
+                self.diff.tablesWithDiff:
             decls.extend(self.getTableDefn(table))
 
         upgradeCommands, downgradeCommands = [], []

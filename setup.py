8252703f56d394743199e702c38f1ecc1db590e5
Matt Riedemann <mriedem@us.ibm.com>
2015-07-15 09:02:50 -0700

    Unblock migrate (py26 and py3* testing issues)
    There are two changes which have to go together to pass the gate
    tests:
    
    1. Update pbr and mock requirements from global-requirements
    
    mock 1.2 supports py26 again so make that the minimum version. The
    same change is being made in g-r with:
    
    Ic6b9e18eaec9c81bbbbc57129e024904be928e09
    
    Sync up with latest pbr in global-requirements while we're at it.
    
    Closes-Bug: #1474925
    
    2. Fix the importpath module to work with python >= 3.3 where the
    __import__ built-in is raising an ImportError on a temporary file
    that is added to the system path.
    
    Closes-Bug: #1475339
    
    Change-Id: Ie98938ba75f3983094dd540b7d26a7ec46be4f6e

--- setup.py
+++ setup.py
@@ -16,6 +16,14 @@
 
 import setuptools
 
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
+
 setuptools.setup(
-    setup_requires=['pbr'],
+    setup_requires=['pbr>=1.3'],
     pbr=True)
